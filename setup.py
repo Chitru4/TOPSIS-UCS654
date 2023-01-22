f93738d32b161ecb104d76d2e009f69d9301f9d4
Chitraksh Kumar <chitraksh24@gmail.com>
2023-01-22 22:51:20 +0530

    Finally

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'TOPSIS',         # How you named your package folder (MyLib)
-  packages = ['TOPSIS'],   # Choose the same as "name"
+  name = 'Topsis-Chitraksh-102017040',         # How you named your package folder (MyLib)
+  packages = ['Topsis-Chitraksh-102017040'],   # Choose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library is used to run TOPSIS on given data to rank items',   # Give a short description about your library
