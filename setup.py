0c38aacedc1036c9905a488aed99e02daf784322
Chitraksh Kumar <chitraksh24@gmail.com>
2023-01-22 23:29:33 +0530

    added readme

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name = 'Topsis-Chitraksh-102017040',        
   packages = ['Topsis-Chitraksh-102017040'],   
